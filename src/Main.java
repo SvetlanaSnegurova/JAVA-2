7788a4d4c83d1e10e9c7d09dc8138ac0ea998a49
Svetlana Lazareva <slazareva.sl@gmail.com>
2023-01-28 14:36:32 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int account = 200;
-        int deposit = 1100;
+        int deposit = 900;
 
-        int bonus;
-        if ( deposit >= 1000 ) {
+        int bonus = 0;
+        if ( deposit > 1000 ) {
             bonus = deposit / 100;
         }
-        else {
-            bonus = 0;
-        }
         int amout = account + deposit + bonus;
-
         System.out.println("Итоговый бонус: " + bonus);
         System.out.println("Итоговая сумма на счете: " + amout);
     }
